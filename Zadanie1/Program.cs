cb4f1f85a0da53ef4f6ee2b7659920613548e9c5
janogrzewalski <s20406@pjwstk.edu.pl>
2024-03-10 18:37:53 +0100

    Modyfikacja 3

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,7 +1,13 @@
 ﻿public class Task1
 {
+    
     public static void Main()
     {
         Console.WriteLine("APBD Zadanie 1");
+        List<string> names = ["<name>", "Ana", "Felipe"];
+        foreach (var name in names)
+        {
+            Console.WriteLine($"Hello {name.ToUpper()}!");
+        }
     }
 }
